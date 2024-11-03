81ef8f2a31abb637fbabb399db835aa75850b0ad
shangcom <dndbs91@gmail.com>
2024-11-03 21:25:36 +0900

    스프링 핵심 원리 - 고급편
    섹션13 - 스프링 AOP - 실전 예제
    
    로그 출력 AOP
    
    완료

--- src/test/java/hello/aop/exam/ExamTest.java
+++ src/test/java/hello/aop/exam/ExamTest.java
@@ -1,13 +1,16 @@
 package hello.aop.exam;
 
+import hello.aop.exam.aop.TraceAspect;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @Slf4j
 @SpringBootTest
-class ExamServiceTest {
+@Import(TraceAspect.class)
+class ExamTest {
 
     @Autowired
     ExamService examService;
@@ -19,7 +22,7 @@ class ExamServiceTest {
         // TODO: Initialize test data
         for (int i = 0; i < 5; i++) {
             log.info("client request count = {}", i + 1);
-            examService.request("data + i");
+            examService.request("data" + i);
         }
 
         // Act
