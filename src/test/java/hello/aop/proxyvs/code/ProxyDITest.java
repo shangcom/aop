5f527de8241f39ab9f4cdd1096de77ea6014794d
shangcom <dndbs91@gmail.com>
2024-11-04 16:04:42 +0900

    스프링 핵심 원리 - 고급편
    섹션14 - 스프링 AOP - 실무 주의사항
    
    프록시 기술과 한계 - 의존관계 주입
    
    완료

--- src/test/java/hello/aop/proxyvs/code/ProxyDITest.java
+++ src/test/java/hello/aop/proxyvs/code/ProxyDITest.java
@@ -15,6 +15,13 @@ public class ProxyDITest {
     @Autowired
     MemberService memberService;
 
+    /*
+     현재 MemberService 인터페이스 기반의 JDK 동적 프록시만 존재한다.
+     MemberServiceImpl 타입의 프록시는 존재하지 않는다.
+     현재 프록시는 MemberService를 구현했을뿐, MemberServiceImpl과는 무관하며, MemberServiceImpl 타입으로 인식되지 않는다.
+     아래 코드에 의해 프록시가 MemberServiceImpl 대신 주입되어야 하는데, 위의 이유로 타입 불일치가 발생한다.
+     UnsatisfiedDependencyException 발생!
+     */
 //    @Autowired
 //    MemberServiceImpl memberServiceImpl;
 
