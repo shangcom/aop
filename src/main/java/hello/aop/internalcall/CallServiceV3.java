681ff0ca7da111258c76059687d70a4ab77e4127
shangcom <dndbs91@gmail.com>
2024-11-04 14:15:45 +0900

    스프링 핵심 원리 - 고급편
    섹션14 - 스프링 AOP - 실무 주의사항
    
    프록시와 내부 호출 - 대안3 구조 변경
    
    완료

---
+++ src/main/java/hello/aop/internalcall/CallServiceV3.java
@@ -0,0 +1,23 @@
+package hello.aop.internalcall;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * 구조 변경(분리)
+ * internal() 메서드를 새로운 클래스 InternalService에 작성하고 주입받아 사용.
+ */
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class CallServiceV3 {
+
+    private final InternalService internalService;
+
+    public void external() {
+        log.info("call external");
+        internalService.internal();
+    }
+
+}
